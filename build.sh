f5e5d2edafb795a40699feddf4f8d89607e24a3e
Connor Clark <cjamcl@gmail.com>
2022-02-20 23:23:28 -0800

    make zc_quests dir if necessary

--- build.sh
+++ build.sh
@@ -14,4 +14,5 @@ yarn parcel build src/index.html
 cp decode_zc/dist/zc.wasm dist/zc.wasm
 
 ln -s $PWD/tmp/ $PWD/dist/tmp
+mkdir -p $PWD/dist/tmp/zc_quests/
 cp -r $PWD/data/zc_quests/* $PWD/dist/tmp/zc_quests/
