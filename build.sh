c02913b3557856d401f5a5b713b850fead4f1cc1
Connor Clark <cjamcl@gmail.com>
2022-02-22 20:56:47 -0800

    remove tmp from quest path

--- build.sh
+++ build.sh
@@ -9,10 +9,8 @@ cp -r data/debug dist/quests
 cp node_modules/timidity/libtimidity.wasm dist
 cp -r node_modules/freepats/* dist
 
-yarn parcel build src/index.html
-
 cp decode_zc/dist/zc.wasm dist/zc.wasm
 
-ln -s $PWD/tmp/ $PWD/dist/tmp
-mkdir -p $PWD/dist/tmp/zc_quests/
-cp -r $PWD/data/zc_quests/* $PWD/dist/tmp/zc_quests/
+rsync --ignore-existing $PWD/data/zc_quests/ $PWD/tmp/zc_quests/
+
+ln -nfs $PWD/tmp/zc_quests $PWD/dist/zc_quests
