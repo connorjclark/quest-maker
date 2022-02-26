9e7a65334049ffce7ec83ef9987deda8a3bdf55e
Connor Clark <cjamcl@gmail.com>
2022-02-25 22:14:42 -0800

    fix issue where deploying makes wav files not be served anymore on hoten.cc

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,3 +7,4 @@ yarn
 yarn build
 ssh root@hoten.cc "mkdir -p /var/www/hoten.cc/public_html/quest-maker/play/"
 rsync -ahvzL --delete ./dist/ root@hoten.cc:/var/www/hoten.cc/public_html/quest-maker/play/
+ssh root@hoten.cc "sudo chown -R www-data:www-data /var/www/hoten.cc/"
