4793f83ba340d55b15de6d1db771aed52836bd61
Connor Clark <cjamcl@gmail.com>
2022-02-28 23:59:50 -0800

    room type enum

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,27 @@ export const tileSize = 16;
 export const screenWidth = 16;
 export const screenHeight = 11;
 export const numLayers = 7;
+export enum RoomType {
+  RT_DOORREPAIR = 5,
+  RT_FEEDTHEGORIYA = 7,
+  RT_GAMBLE = 4,
+  RT_GANON = 15,
+  RT_ITEMPOND = 17,
+  RT_LEARNSLASH = 19,
+  RT_LEVEL9ENTRANCE = 8,
+  RT_MAGICUPGRADE = 18,
+  RT_MONEYORLIFE = 12,
+  RT_MOREARROWS = 20,
+  RT_MOREBOMBS = 11,
+  RT_NONE = 0,
+  RT_PAYFORINFO = 2,
+  RT_POTIONORHEART = 6,
+  RT_POTIONSHOP = 9,
+  RT_SECRETMONEY = 3,
+  RT_SHOP = 10,
+  RT_SPECIALITEM = 1,
+  RT_STAIRWARP = 14,
+  RT_TAKEONEITEM = 21,
+  RT_TENRUPEES = 13,
+  RT_ZELDA = 16,
+};
